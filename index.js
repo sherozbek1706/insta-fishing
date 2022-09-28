33123894db148adb49f34dc498314170e688f3d9
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2022-09-28 18:01:21 +0500

    make it better

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.set("views", `${__dirname}/views`);
 
 mongoose.connect(
